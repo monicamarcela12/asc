66c686c7005473cafd59c4e58d1a7d308ec75a4f
monicamarcela12 <monicamarcela92021097@gmail.com>
2022-03-31 09:49:34 -0300

    ok

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('ASC'));
+app.use(express.static('asc'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'ASC'}),
+    res.sendFile('index.html', {root: 'asc'}),
 );
 
 // Start the app by listening on the default Heroku port
