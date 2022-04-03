8750e314b5d0e0c0f002fd4ccbb36a086a632409
monicamarcela12 <monicamarcela92021097@gmail.com>
2022-04-03 11:25:32 -0300

    ok

--- src/app/core/services/login.service.ts
+++ src/app/core/services/login.service.ts
@@ -37,7 +37,7 @@ export class LoginService {
   }
 
   registro(data): Observable<any>{
-    return this.http.post(`${API}/usuario/cadastrar`,data)
+    return this.http.post(`${API}/usuario`,data)
   }
 
   logout() {
